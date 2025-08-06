728140bf952017cab3df1b14e703e491883eb66b
Wei <t940408wu@gmail.com>
2025-08-06 19:11:16 +0800

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const authRoutes = require('./user');
 const bodyParser = require('body-parser');
 const cors = require('cors');
